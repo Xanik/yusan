dc93e9e4f42ae200d4376849364153d534175c7b
UsufAbdul <yusufabdulkadir29@gmail.com>
2018-03-20 22:41:33 -1100

    describing my self

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 
-	<p>i am a girl</p>
+	<p>i am a boy</p>
+	<p>i am the gentle type
 	<p>THis is me, look out for here i come</p>
 	<p>i'm not a stranger to the dark</p>
 	<h2>Hide away they say, we don't want this broken parts.</h2>
