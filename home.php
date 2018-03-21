615fe61fe7e4f6347df69297593bd8a2ec22f1ab
Xanik <sandraike57@gmail.com>
2018-03-21 10:21:47 +0100

    Home Page

---
+++ home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>This is the day that the lord has made</title>
+</head>
+<body>
+
+	<p>i will rejoice and be glad he lives</p>
+	<p>got my new harddrive</p>
+	
+
+</body>
+</html>
